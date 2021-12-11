ef4821b92ec7243bb6164ac324d9caa34da8f416
Andre Ribeiro <andre.ribeiro.srs@gmail.com>
2021-12-11 14:31:26 -0300

    Values fetched.

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from threading import ThreadError, Thread
 
 # Added modules
 from pytictoc import TicToc
+from bs4 import BeautifulSoup
+import requests
 
 # Project modules
 
@@ -22,6 +24,12 @@ logging.basicConfig(level=logging.INFO,
                     datefmt='%d/%b/%Y - %H:%M:%S')
 
 logger = logging.getLogger(__name__)
+
+
+# specifying user agent, You can use other user agents
+# available on the internet
+HEADERS = ({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/44.0.2403.157'
+                          'Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
 
 
 class ElapsedTime(object):
@@ -71,3 +79,39 @@ def run(interval):
 def application():
     """" All application has its initialization from here """
     logger.info('Main application is running!')
+
+    url = 'https://www.amazon.com.br/dp/B002VBV1R2/?coliid=I2FWYFEM75XDPB&colid=2IGPTPK62VKE4&psc=0&ref_=lv_ov_lig_dp_it'
+
+    # Making the HTTP Request
+    webpage = requests.get(url, headers=HEADERS)
+
+    # Creating the Soup Object containing all data
+    soup = BeautifulSoup(webpage.content, "lxml")
+
+    # retrieving product title
+    try:
+        # Outer Tag Object
+        title = soup.find("span",
+                          attrs={"id": 'productTitle'})
+
+        # Inner NavigableString Object
+        title_value = title.string
+
+        # Title as a string value
+        title_string = title_value.strip().replace(',', '')
+        logger.info("product Title = {}".format(title_string))
+
+    except AttributeError as e:
+        logger.exception(e, exc_info=False)
+
+    # retrieving price
+    try:
+        price = soup.find("span", attrs={'id': 'price'}).string.strip().replace(',', '')
+        price = price[:-2] + ',' + price[-2:]
+        logger.info(("Products price = {}".format(price)))
+
+    except AttributeError as e:
+        logger.exception(e, exc_info=False)
+
+
+
